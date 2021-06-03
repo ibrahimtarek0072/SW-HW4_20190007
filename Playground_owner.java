48ebc6094a8bcf1768cbdde0dd0b3efefde42e90
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-06-03 17:49:27 +0200

    Update Playground_owner.java

--- Playground_owner.java
+++ Playground_owner.java
@@ -1,18 +1,26 @@
 public class Playground_owner {
-    private String name,Email,location,state,phone_num;
+    private String Fname,Lname,Email,paasword,Confirmpass,location;
     private int age,nationlID;
+    public Playground_info p=new Playground_info();
 
     public void setAge(int age) {
         this.age = age;
     }
-    public Playground_owner(String name,String email,String location,String state,int age,String phone,int nationalID){
+    public Playground_owner(String name,String email,String location,int age,String phone,int nationalID,String lname,String pass,String conpass){
         this.age=age;
         this.Email=email;
         this.location=location;
-        this.name=name;
-        this.phone_num=phone;
+        this.Fname=name;
+        this.Lname=lname;
         this.nationlID=nationalID;
-        this.state=state;
+        this.paasword=pass;
+        this.Confirmpass=conpass;
+        if(conpass!=pass){
+            System.out.println("the password don't much");
+            System.exit(1);
+
+        }
+
     }
     public Playground_owner(){
 
@@ -26,26 +34,12 @@ public class Playground_owner {
         this.location = location;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFName(String fname) {
+        this.Fname = fname;
     }
-
-    public void setPhone_num(String phone_num) {
-        this.phone_num = phone_num;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
     public int getAge() {
         return age;
     }
-
-    public String getPhone_num() {
-        return phone_num;
-    }
-
     public String getEmail() {
         return Email;
     }
@@ -54,30 +48,72 @@ public class Playground_owner {
         return location;
     }
 
-    public String getName() {
-        return name;
+    public String getFname() {
+        return Fname;
     }
 
-    public String getState() {
-        return state;
-    }
 
     public void setNationlID(int nationlID) {
         this.nationlID = nationlID;
     }
 
+    public void setLname(String lname) {
+        Lname = lname;
+    }
+
+    public void setPaasword(String paasword) {
+        this.paasword = paasword;
+    }
+
+    public void setConfirmpass(String confirmpass) {
+        Confirmpass = confirmpass;
+        if(this.paasword!=confirmpass){
+            System.out.println("password don't much");
+            System.exit(1);
+        }
+    }
+
+    public String getLname() {
+        return Lname;
+    }
+
     public int getNationlID() {
         return nationlID;
     }
+      public void check(){
+        if(this.paasword!=this.Confirmpass){
+            System.out.println("password don't much");
+            System.exit(1);
+        }
+     }
 
     public void display(){
         System.out.println("your info is:");
-        System.out.println("Name:"+getName());
+        System.out.println("Frist Name:"+getFname());
+        System.out.println("Last name: "+getLname());
         System.out.println("Age: "+getAge());
         System.out.println("Location: "+getLocation());
         System.out.println("Email: "+getEmail());
-        System.out.println("State:"+getState());
-        System.out.println("Phone: "+getPhone_num());
         System.out.println("National ID: "+getNationlID());
+    }
+    public void addplayground(Playground_owner o){
+        this.p.playgrounds.add(this.p);
+        this.p.addPlayground(o);
+
+    }
+    public void displayPlaygrounds(){
+        this.p.displayplaygrounds();
+    }
+    public static void main(String []args){
+        Playground_owner o=new Playground_owner();
+        o.setFName("ibrahim");
+        o.setLname("tarek");
+        o.setAge(20);
+        o.setEmail("ibrahimtarek00072@gmail.com");
+        o.setLocation("cairo");
+        o.setPaasword("01146520613");
+        o.setConfirmpass("01146520613");
+        o.addplayground(o);
+        o.displayPlaygrounds();
     }
 }
