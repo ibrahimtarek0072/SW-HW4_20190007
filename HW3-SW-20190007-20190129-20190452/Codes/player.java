ab410807c3846db373f88392a709d2b32662f9f3
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-06-13 02:02:21 +0200

    Update player.java

--- HW3-SW-20190007-20190129-20190452/Codes/player.java
+++ HW3-SW-20190007-20190129-20190452/Codes/player.java
@@ -1,5 +1,4 @@
-import sun.security.util.Password;
-
+//@Mohamed Adel Abdallah
 import java.util.Scanner;
 
 public class player {
@@ -20,50 +19,52 @@ public class player {
             System.exit(1);
         }
     }
-
+    ///setters
     public void setEmail(String email) {
         Email = email;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public void setFristName(String fristName) {
         this.fristName = fristName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public void setPassword(String password) {
         Password = password;
     }
+
+    //getters
     public String getEmail() {
         return Email;
     }
-
     public String getFristName() {
         return fristName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public int getAge() {
         return age;
     }
+
+    //display function to display info
     public void display(){
         System.out.println("Name: "+getFristName()+" "+getLastName());
         System.out.println("Age:"+getAge());
         System.out.println("Email: "+getEmail());
     }
+    // join team function that take a variable it's type is player
     public void jointeam(player pl){
+        //create a join variable from the class join
         Join j=new Join();
+        //called the jointeam function in the class join and make it do it's job
         j.jointeam(pl);
     }
+
+    //this is the main function that will be used in the GoFo class
     public void addPlayer(player p){
         System.out.println("Enter your first name:");
         String name=scan.nextLine();
