63b976f280bc3e9b76c27b21601fe7059e6a7cbe
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-06-14 21:11:03 +0200

    Update player.java

--- HW3-SW-20190007-20190129-20190452/Codes/player.java
+++ HW3-SW-20190007-20190129-20190452/Codes/player.java
@@ -1,6 +1,9 @@
-//@Mohamed Adel Abdallah
 import java.util.Scanner;
 
+/**
+ * this is the player class that will be used by the players
+ * @author Mohamed Adel Abdallah
+ */
 public class player {
     Scanner scan=new Scanner(System.in);
     private String fristName,lastName,Email,Password;
@@ -19,52 +22,96 @@ public class player {
             System.exit(1);
         }
     }
-    ///setters
+
+    /**
+     * set function to set the email
+     * @param email
+     */
     public void setEmail(String email) {
         Email = email;
     }
+
+    /**
+     * set function to set the age
+     * @param age
+     */
     public void setAge(int age) {
         this.age = age;
     }
+
+    /**
+     * set function to set the frist name
+     * @param fristName
+     */
     public void setFristName(String fristName) {
         this.fristName = fristName;
     }
+
+    /**
+     * set function to set the last name
+     * @param lastName
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+    /**
+     * set method to set the password
+     * @param password
+     */
     public void setPassword(String password) {
         Password = password;
     }
 
-    //getters
+    /**
+     * get function to return the email
+     * @return Email
+     */
     public String getEmail() {
         return Email;
     }
+
+    /**
+     * get function to return the first name
+     * @return firstName
+     */
     public String getFristName() {
         return fristName;
     }
+
+    /**
+     * get function to return the last name
+     * @return lastName
+     */
     public String getLastName() {
         return lastName;
     }
+
+    /**
+     * get function to return the age
+     * @return age
+     */
     public int getAge() {
         return age;
     }
 
-    //display function to display info
+    /**
+     * display function
+     */
     public void display(){
         System.out.println("Name: "+getFristName()+" "+getLastName());
         System.out.println("Age:"+getAge());
         System.out.println("Email: "+getEmail());
     }
-    // join team function that take a variable it's type is player
     public void jointeam(player pl){
-        //create a join variable from the class join
         Join j=new Join();
-        //called the jointeam function in the class join and make it do it's job
         j.jointeam(pl);
     }
 
-    //this is the main function that will be used in the GoFo class
+    /**
+     * the main function that will be used in the GoFo class
+     * @param p
+     */
     public void addPlayer(player p){
         System.out.println("Enter your first name:");
         String name=scan.nextLine();
@@ -83,6 +130,11 @@ public class player {
         a=scan.nextInt();
         p.age=a;
     }
+
+    /**
+     * the main function
+     * @param args
+     */
     public static void main(String[]args){
         player p=new player();
         p.addPlayer(p);
