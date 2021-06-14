680162930ba8ce5e26be55dd185b880d48a59530
ibrahimtarek0072 <81970421+ibrahimtarek0072@users.noreply.github.com>
2021-06-14 21:11:05 +0200

    Update Playground_info.java

--- HW3-SW-20190007-20190129-20190452/Codes/Playground_info.java
+++ HW3-SW-20190007-20190129-20190452/Codes/Playground_info.java
@@ -1,73 +1,144 @@
 import java.util.*;
 
-//@Bassem yasser ده الجزء بتاعك ياعلق
+/**
+ * play ground info class that have functions to add play ground
+ * @author Bassem yasser taha
+ */
 public class Playground_info {
+    /**
+     * the class have two int variable, three float variable, two string variable and playground_owner variable
+     */
     Scanner scan=new Scanner(System.in);
     private int player_num;
     private float open_hur,close_hur,pric;
     private String location,playground_name;
     private Playground_owner owner;
-    //crate an ArryList to hold the playgrounds info to display it
     public ArrayList<Playground_info> playgrounds=new ArrayList<Playground_info>();
-    // setters
     public void setLocation(String location) {
         this.location = location;
     }
+
+    /**
+     * set method to set the closing hour
+     * @param close_hur
+     */
 
     public void setClose_hur(float close_hur) {
         this.close_hur = close_hur;
     }
 
+    /**
+     * set method to set the open hour
+     * @param open_hur
+     */
+
     public void setOpen_hur(float open_hur) {
         this.open_hur = open_hur;
     }
+
+    /**
+     * set method to set the owner
+     * @param owner
+     */
 
     public void setOwner(Playground_owner owner) {
         this.owner = owner;
     }
 
+    /**
+     * set method to set the number of the players
+     * @param player_num
+     */
     public void setPlayer_num(int player_num) {
         this.player_num = player_num;
     }
+
+    /**
+     * set method to set the playground name
+     * @param playground_name
+     */
     public void setPlayground_name(String playground_name) {
         this.playground_name = playground_name;
     }
+
+    /**
+     * set method to set the price
+     * @param pric
+     */
     public void setPric(float pric) {
         this.pric = pric;
     }
 
+    /**
+     * get method to return the location
+     * @return location
+     */
 
-    //getters
     public String getLocation() {
         return location;
     }
+
+    /**
+     * get method to return closing hour
+     * @return close_hur
+     */
     public float getClose_hur() {
         return close_hur;
     }
+
+    /**
+     * get method to return open hour
+     * @return open_hur
+     */
     public float getOpen_hur() {
         return open_hur;
     }
+
+    /**
+     * get method to return the player number
+     * @return player_num
+     */
     public int getPlayer_num() {
         return player_num;
     }
+
+    /**
+     * get method to return the owner
+     * @return owner
+     */
     public Playground_owner getOwner() {
         return owner;
     }
+
+    /**
+     * get method to return the price
+     * @return price
+     */
     public float getPric() {
         return pric;
     }
+
+    /**
+     * get method to return the play ground name
+     * @return playground_name
+     */
     public String getPlayground_name() {
         return playground_name;
     }
 
 
-
-    //Modify function to modify playgrounds price
+    /**
+     * this method used to modify the price
+     * @param price
+     */
     public void modifyPrice(int price){
         this.pric=price;
     }
 
-        //display function to display the playground info
+    /**
+     * display function to display the playgrounds
+     */
+
         public void display(){
         owner.display();
         System.out.println("Player numbers:"+getPlayer_num());
@@ -77,7 +148,11 @@ public class Playground_info {
         System.out.println("Price ber Hour: "+getPric());
         System.out.println("Playground name: "+getPlayground_name());
     }
-    // add playground this is the main function that hold all the class and the function we used in the main class GoFo
+
+    /**
+     * addplayground is the main function that will be used in the GoFo
+     * @param owner
+     */
     public void addPlayground(Playground_owner owner){
         Playground_info p=new Playground_info();
         p.owner=owner;
@@ -113,7 +188,10 @@ public class Playground_info {
         p.playgrounds.add(p);
 
     }
-    // display the storing playground in data base ده لو عاملين داتا بيز اصلا
+
+    /**
+     * display the storing playground that stored in the data base in any
+     */
     public void displayplaygrounds(){
             for(int i=0;i<playgrounds.size();i++){
                 System.out.println("Playgrouns: "+playgrounds.get(i));
